36ae7e24e0eca43240c39334aee69535c2eeb6ef
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-07-26 19:44:54 +0930

    Begin layout and styling of tumblr theme.

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 module AdVault
   class App < Sinatra::Base
     get '/' do
-      haml "%h1 Hello!"
+      haml :mockup, :layout => false
     end
-    
+
     get '/spending.html' do
       haml "%h1 Spending"
     end
-    
+
     get '/ads.html' do
       haml "%h1 ads"
     end
@@ -15,11 +15,11 @@ module AdVault
     get '/candidates.html' do
       haml "%h1 Candidates\n<ul>#{Candidate.map{|c|"<li>#{c.name}</li>"}.join}</ul>"
     end
-    
+
     get '/candidates/:slug.html' do
       haml "%h1 Candidate #{Candidate.first(:slug=>params[:slug]).name}"
     end
-    
+
     get '/candidates/:slug/spending.html' do
       haml "%h1 Spending for Candidate #{Candidate.first(:slug=>params[:slug]).name}"
     end
@@ -27,7 +27,7 @@ module AdVault
     get '/candidates/:slug/ads.html' do
       haml "%h1 Ads for Candidate #{Candidate.first(:slug=>params[:slug]).name}"
     end
-    
+
     get '/offices.html' do
       haml "%h1 Office"
     end
@@ -43,7 +43,7 @@ module AdVault
     get '/offices/:slug/ads.html' do
       haml "%h1 Office"
     end
-    
+
     get '/stations.html' do
       haml "%h1 Station"
     end
@@ -59,7 +59,7 @@ module AdVault
     get '/stations/:slug/ads.html' do
       haml "%h1 Station"
     end
-    
+
     get '/advertisers.html' do
       haml "%h1 Advertiser"
     end
@@ -76,14 +76,14 @@ module AdVault
       haml "%h1 Advertiser"
     end
   end
-  
+
   class Admin < Sinatra::Base
     get '/' do
     end
-    
+
     get '/share' do
     end
-    
+
     get '/shared' do
     end
   end
